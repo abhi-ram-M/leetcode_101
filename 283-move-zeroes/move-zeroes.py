6f5ad24a24de00fb26174ebde71c596ec16ddcf1
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-07-19 14:27:49 +0530

    Time: 3 ms (80.17%) | Memory: 18.7 MB (79.05%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,13 +5,13 @@ class Solution:
         """
         n = len(nums)
         if (1 <= n <= 10**4):
-            temp = []
-            for i in range(n):
+            count=0
+        for i in range(n):
                 if nums[i] != 0:
-                    temp.append(nums[i])
-            nz = len(temp)
-            for j in range(nz):
-                nums[j] = temp[j]
-            for k in range(nz,n):
-                nums[k] = 0
+                    nums[count] = nums[i]
+                    count += 1
+        for i in range(count, n):
+                nums[i] = 0
+                            
+                    
         
