5ff8f21fe01ec377a29669678c76ee04d746dd67
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-07-19 21:37:40 +0530

    Time: 47 ms (59.97%) | Memory: 41.9 MB (88.03%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = [0]*n
+        p,n = 0,1
+        for i,e in enumerate(nums):
+            if e>0:
+                res[p] = e
+                p += 2
+            else:
+                res[n] = e
+                n+=2
+        return res
+
+
+
