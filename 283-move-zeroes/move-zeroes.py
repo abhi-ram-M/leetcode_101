709af0f66162dee1877f810ce2665a48a6792281
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-07-19 14:30:49 +0530

    Time: 1 ms (87.48%) | Memory: 18.8 MB (79.05%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,14 +4,14 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        if (1 <= n <= 10**4):
-            count=0
-            for i in range(n):
-                    if nums[i] != 0:
-                        nums[count] = nums[i]
-                        count += 1
-            for i in range(count, n):
-                    nums[i] = 0
+
+        count=0
+        for i in range(n):
+                if nums[i] != 0:
+                    nums[count] = nums[i]
+                    count += 1
+        for i in range(count, n):
+                nums[i] = 0
                             
                     
         
