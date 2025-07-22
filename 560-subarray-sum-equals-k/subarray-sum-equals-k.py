074081642cf286cf2f3f40f0ddcb6cf6c80c1375
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-07-22 14:02:44 +0530

    Time: 31 ms (63.75%) | Memory: 20.3 MB (71.46%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -5,14 +5,16 @@ class Solution:
         if (1 <= n <= 2 * 10**4):
             res = 0
             prefix_sums = {0:1}
-            cur_sum = 0
+            curr_sum = 0
             for num in nums:
-                cur_sum += num
-                diff = cur_sum - k
+                curr_sum += num
+                diff = curr_sum -k
                 res += prefix_sums.get(diff,0)
-                prefix_sums[cur_sum] = prefix_sums.get(cur_sum,0)+1
+                prefix_sums[curr_sum] = prefix_sums.get(curr_sum,0)+1
             return res
+                
 
+        
 
             
 
