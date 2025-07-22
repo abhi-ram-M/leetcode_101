f81c9baa5c004418383609772d0f54d742f22fac
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-07-22 23:35:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (28.17%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -3,25 +3,30 @@ class Solution:
         if (1 <= len(s) <= 500):
             import heapq
             char_count = {}
-            res = []
-            for char in s:
-                char_count[char] = char_count.get(char,0)+1
             max_heap = []
-            for char,count in char_count.items():
-                if count > (len(s)+1)//2:
+            res = []
+            for ch in s:
+                if ch in char_count:
+                    char_count[ch] += 1
+                else:
+                    char_count[ch] = 1
+            for k,v in char_count.items():
+                if v > (len(s)+1)//2:
                     return ''
-                max_heap.append((-count,char))
+                max_heap.append([-v,k])
+            prev_count,prev_value = 0,''
             heapq.heapify(max_heap)
-            prev_count = 0
-            prev_char = ''
             while max_heap:
-                count,char = heapq.heappop(max_heap)
-                res.append(char)
+                curr_count,curr_value = heapq.heappop(max_heap)
+                res.append(curr_value)
                 if prev_count < 0:
-                    heapq.heappush(max_heap,(prev_count,prev_char))
-                prev_count= count+1
-                prev_char =char
+                    heapq.heappush(max_heap,[prev_count,prev_value])
+                prev_count = curr_count +1
+                prev_value = curr_value
             return ''.join(res)
+
+
+            
 
             
 
