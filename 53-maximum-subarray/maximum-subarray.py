f7d1b4b8d9ebbd84ebd898d4a60c24d9277ca74f
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-07-22 11:23:46 +0530

    Time: 44 ms (83.94%) | Memory: 32.9 MB (6.42%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
-        if 1 <= n <= 10**5:
-            maxi = float('-inf')
-            total = 0
-            for i in range(0,n):
-                total += nums[i]
-                maxi = max(total,maxi)
-                if total<0:
-                    total = 0
-            return maxi
+        maxi = float('-inf')
+        total = 0
+        for i in nums:
+            total += i
+            maxi = max(maxi,total)
+            if total <= 0:
+                total = 0
+        return maxi
             
