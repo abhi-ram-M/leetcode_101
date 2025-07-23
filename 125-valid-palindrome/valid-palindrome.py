307bd6660b2eccea00c5bcb6f849e4380793599d
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-07-23 16:13:44 +0530

    Time: 15 ms (13.50%) | Memory: 18 MB (83.17%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,19 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
+        n = len(s)
+        l = 0
+        r = n-1
         while l<r:
-            while l<r and not self.alp(s[l]):
-                l += 1
-            while l<r and not self.alp(s[r]):
-                r -= 1
-            if s[l].lower() != s[r].lower():
+            while l<r and not self.is_as(s[l]):
+                l+=1
+            while l<r and not self.is_as(s[r]):
+                r-=1
+            if s[l].lower()!=s[r].lower():
                 return False
-            l += 1
-            r -= 1
+            l+=1
+            r-=1
         return True
-    
 
 
-    def alp(self,c):
-        return ('A'<=c<='Z') or ('0'<=c<='9') or ('a'<=c<='z')
-
+    def is_as(self,c):
+        return ('a'<=c<='z') or ('A'<=c<='Z') or ('0'<=c<='9')
