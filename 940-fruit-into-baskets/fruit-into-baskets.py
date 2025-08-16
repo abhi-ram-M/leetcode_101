90366a3cb51d2247a89ebb8828e3fd1a44587b29
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-08-16 15:11:44 +0530

    Time: 134 ms (81.53%) | Memory: 23.2 MB (98.16%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -8,7 +8,7 @@ class Solution:
             r = 0
             while r<n:
                 dic[fruits[r]] = dic.get(fruits[r],0)+1
-                while len(dic) > 2:
+                if len(dic) > 2:
                     dic[fruits[l]] -= 1
                     if dic[fruits[l]] == 0:
                         del dic[fruits[l]]
