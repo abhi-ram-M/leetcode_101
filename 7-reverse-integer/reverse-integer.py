169347c07b0edc4a7296f5888869d6109ef133e6
abhi-ram-M <129652453+abhi-ram-M@users.noreply.github.com>
2025-09-17 10:47:33 +0530

    Time: 15 ms (76.01%) | Memory: 12.5 MB (7.58%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,16 +1,13 @@
 class Solution(object):
     def reverse(self, x):
-        if x == 0:
+        if -2**31 <= x <= 2**31 - 1 and x != 0:
+            sign = -1 if x < 0 else 1
+            x = abs(x)
+            y = 0
+            while x != 0:
+                y = y * 10 + x % 10
+                x = x // 10
+            y = sign * y
+            return y if -2**31 <= y <= 2**31 - 1 else 0
+        else:
             return 0
-        sign = -1 if x < 0 else +1
-        x = abs(x)
-        y = 0
-        while x>0:
-            y = (y*10) +(x%10)
-            x = x//10
-        if y>2**31:
-            return 0
-        return y*sign
-    
-    
-        
