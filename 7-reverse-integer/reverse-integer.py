4236ae036797718afeefebe0bea7e8ee268d3d19
Abhiram Manepalli <129652453+abhi-ram-M@users.noreply.github.com>
2026-03-31 13:37:08 +0530

    Time: 24 ms (16.99%) | Memory: 12.5 MB (35.31%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -3,11 +3,9 @@ class Solution(object):
         sign = -1 if x < 0 else 1
         x = abs(x)
         y = 0
-
         while x:
-            y = y * 10 + x % 10
-            x //= 10
-
+            y = y*10 + (x%10)
+            x = x//10
         y *= sign
 
         if -2**31 <= y <= 2**31 - 1:
