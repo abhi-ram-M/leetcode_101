6e24e1127f46b310747581e4e5aaa34a3b9e2f27
Abhiram Manepalli <129652453+abhi-ram-M@users.noreply.github.com>
2026-05-26 10:21:14 +0530

    Time: 17 ms (26.47%) | Memory: 24 MB (9.96%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,11 +4,12 @@ class Solution:
         dic = defaultdict(list)
         for word in strs:
             lst = [0]*26
-            for char in word:
-                lst[ord(char)-ord('a')] += 1
+            for letter in word:
+                lst[ord(letter)-ord('a')] += 1
             lst = tuple(lst)
             dic[lst].append(word)
         return list(dic.values())
+
 
 
 
