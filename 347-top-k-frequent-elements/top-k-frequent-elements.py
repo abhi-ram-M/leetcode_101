b12c022b029d015a9fdb3ef4a977501dba148272
Abhiram Manepalli <129652453+abhi-ram-M@users.noreply.github.com>
2026-05-28 23:19:10 +0530

    Time: 7 ms (51.80%) | Memory: 22.9 MB (61.94%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -4,9 +4,9 @@ class Solution:
         for num in nums:
             mapp[num] = mapp.get(num,0)+1
         heap = []
-        for key,val in mapp.items():
+        for key, val in mapp.items():
             heapq.heappush(heap,[val,key])
             if len(heap)>k:
-                heapq.heappop(heap)         
+                heapq.heappop(heap)
         return [i[1] for i in heap]
         
