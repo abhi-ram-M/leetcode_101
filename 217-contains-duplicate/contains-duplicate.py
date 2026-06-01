fb0ee2c4f5980745d4eb400d158cfcde37843609
Abhiram Manepalli <129652453+abhi-ram-M@users.noreply.github.com>
2026-06-01 22:55:33 +0530

    Time: 5 ms (91.37%) | Memory: 32.3 MB (27.28%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,13 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        n = len(nums)
-        if 1 <= n <= 10**5:
-            mapp = {}
-            for i in range(n):
-                if nums[i] in mapp:
-                    return True
-                mapp[nums[i]] = mapp.get(nums[i],0)+1
-            return False
+        s = set()
+        for i in nums:
+            if i in s:
+                return True
+            else:
+                s.add(i)
+        return False
             
 
         
