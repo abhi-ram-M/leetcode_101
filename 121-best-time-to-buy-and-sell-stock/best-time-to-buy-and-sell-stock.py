c688ecf860eba2af4c61a55630ede59ffd7326c3
Abhiram Manepalli <129652453+abhi-ram-M@users.noreply.github.com>
2026-06-03 22:21:19 +0530

    Time: 72 ms (13.87%) | Memory: 28.6 MB (75.71%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
-        __import__("atexit").register(lambda: open("memory_usage.txt", "w").write("0 MB"))
-        n =  len(prices)
-        min_price = float('inf')
-        max_profit = 0
-        for i in prices:
-            if min_price > i :
-                min_price = i
-            if (i-min_price)>max_profit:
-                max_profit = i-min_price
-        return max_profit
+        l,r = 0,1
+        maxP = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP,profit)
+            else:
+                l = r
+            r += 1
+        return maxP
