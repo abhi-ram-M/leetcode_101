72dc9ca183243a18ec2c0fe7dad312ec9817ad65
Abhiram Manepalli <129652453+abhi-ram-M@users.noreply.github.com>
2026-07-30 18:08:04 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (40.28%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapp = {}
-        for ind,val in enumerate(nums):
-            k = target - val
-            if k in mapp:
-                return [ind,mapp[k]]
-            mapp[val] = ind
+        n = len(nums)
+        if n >= 2 or n < 10**4:
+            mapp = {}
+            for ind,val in enumerate(nums):
+                k = target - val
+                if k in mapp:
+                    return [ind,mapp[k]]
+                mapp[val] = ind
 
 
